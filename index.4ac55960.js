32e141716c820bd0cd3706970f146f121ed2bf02
Burla4enko <45729176+Burla4enko@users.noreply.github.com>
2022-07-26 16:29:55 +0000

    Deploying to gh-pages from @ Burla4enko/ice-cream-team-project@bcf2a87654a627c436d8365daed97f4709a72937 🚀

---
+++ index.4ac55960.js
@@ -0,0 +1,2 @@
+$(".carousel").slick({dots:!0,arrows:!1,speed:1e3,easing:"ease",autoplay:!0,autoplaySpeed:5e3,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0});
+//# sourceMappingURL=index.4ac55960.js.map
---
+++ index.a8f0e9ba.js
@@ -0,0 +1,2 @@
+$(".carousel").slick({dots:!0,arrows:!1,speed:1e3,easing:"ease",autoplay:!0,autoplaySpeed:5e3,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0});
+//# sourceMappingURL=index.a8f0e9ba.js.map
