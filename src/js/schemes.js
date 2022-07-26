f430c9307784b446f354b1b41091c5884e2c7731
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2022-07-26 03:01:26 +0300

    Add color schemes

---
+++ src/js/schemes.js
@@ -0,0 +1,33 @@
+(() => {
+    const pinkSwitcher = document.querySelector(".schemes__circle--pink");
+    const body = document.querySelector("body");
+
+    pinkSwitcher.addEventListener("click", () => {
+        body.classList.remove("coffee-scheme", "green-scheme");
+    });
+    
+})();
+
+(() => {
+
+    const coffeeSwitcher = document.querySelector(".schemes__circle--coffee");
+    const body = document.querySelector("body");
+
+    
+    coffeeSwitcher.addEventListener("click", () => {
+        body.classList.remove("green-scheme");
+        body.classList.add("coffee-scheme");
+    });
+
+})();
+
+(() => {
+    const greenSwitcher = document.querySelector(".schemes__circle--green");
+    const body = document.querySelector("body");
+    
+    greenSwitcher.addEventListener("click", () => {
+        body.classList.remove("coffee-scheme");
+        body.classList.add("green-scheme");
+
+    });
+})();
